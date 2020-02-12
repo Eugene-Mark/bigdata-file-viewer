dc77fc2ee7344f3fdb7d270f7bf6912737fa0d02
Eugene <gearon@tom.com>
2020-02-12 12:41:15 +0800

    Add icon.

--- src/main/java/org/eugene/App.java
+++ src/main/java/org/eugene/App.java
@@ -3,6 +3,7 @@ package org.eugene;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.eugene.ui.CustomizedMenuBar;
@@ -22,6 +23,7 @@ public class App extends Application
         double width = 800;
         double height = 600;
         Scene scene = new Scene(vbox, width, height);
+        stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("icons8-owl-64.png")));
         stage.setTitle("Bigdata File Viewer");
         stage.setScene(scene);
         stage.setMaximized(true);
