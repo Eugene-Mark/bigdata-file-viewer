10969f6eef4ac169bfaae0b2f7a27b67dc90fb4c
Eugene-Mark <eugene.ma.twenty@gmail.com>
2022-01-08 16:40:02 +0800

    Fix label font can't be displayed correctly

--- src/main/java/org/eugene/FXApplication.java
+++ src/main/java/org/eugene/FXApplication.java
@@ -10,6 +10,8 @@ import org.eugene.persistent.PhysicalDB;
 import org.eugene.ui.CustomizedMenuBar;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.awt.*;
+
 public class FXApplication extends Application {
 
     @Value("${scene-init-width}")
@@ -23,13 +25,15 @@ public class FXApplication extends Application {
         PhysicalDB.getInstance().init();
         MenuBar menuBar = new CustomizedMenuBar(stage);
         VBox vbox = new VBox(menuBar);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth() / 1.2;
+        double height = screenSize.getHeight() / 1.2;
         Scene scene = new Scene(vbox, width, height);
+        scene.getRoot().setStyle("-fx-font-family: 'serif'");
         stage.getIcons().add(new Image(getClass().getClassLoader().getResourceAsStream("icons8-owl-64.png")));
         stage.setTitle("Bigdata File Viewer");
         stage.setScene(scene);
-        stage.setMaximized(true);
         stage.show();
-
     }
 
 }
